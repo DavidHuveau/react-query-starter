05747486c8ed4828e802cb25459a50800f2c59ac
david huveau <davidhuveau@mac171even32.home>
2025-01-20 21:27:19 +0100

    use react query to fetch the data

---
+++ src/Users/UsersList.tsx
@@ -0,0 +1,33 @@
+import { useQuery } from '@tanstack/react-query';
+import { User } from './types';
+
+const fetchUsers = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+};
+
+const UsersList = () => {
+  const { data, error, isLoading, refetch } = useQuery({
+    queryKey: ['users'],
+    queryFn: fetchUsers
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  return (
+    <div>
+      <button onClick={() => refetch()}>Refresh</button>
+      <ul>
+        {data.map((user: User) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default UsersList
